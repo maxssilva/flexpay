42c2dc60ff711dc56e5d1ba62781332fb3eb14f1
max <maxssilva@yahoo.com.br>
2025-06-01 16:36:14 -0300

    up project to github

---
+++ src/main/kotlin/com/mss/flexpay/FlexPayApplication.kt
@@ -0,0 +1,11 @@
+package com.mss.flexpay
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlexPayApplication
+
+fun main(args: Array<String>) {
+	runApplication<FlexPayApplication>(*args)
+}
---
+++ src/test/kotlin/com/mss/flexpay/FlexPayApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mss.flexpay
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FlexPayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
