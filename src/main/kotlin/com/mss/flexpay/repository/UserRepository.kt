d7cd2c290c61c7585e3ac7ef2da2daa613091f2e
maxssilva <idd_msiqueira@uolinc.com>
2025-09-03 09:58:23 -0300

    mss-0001 - removendo imports

--- src/main/kotlin/com/mss/flexpay/repository/UserRepository.kt
+++ src/main/kotlin/com/mss/flexpay/repository/UserRepository.kt
@@ -1,6 +1,5 @@
 package com.mss.flexpay.repository
 
-import com.mss.flexpay.dtos.UserRequest
 import com.mss.flexpay.model.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
--- src/main/kotlin/com/mss/flexpay/service/UserService.kt
+++ src/main/kotlin/com/mss/flexpay/service/UserService.kt
@@ -1,13 +1,12 @@
 package com.mss.flexpay.service
 
-import com.mss.flexpay.dtos.UserRequest
 import com.mss.flexpay.model.User
 import java.util.UUID
 
-interface UserService{
+interface UserService {
     fun createUser(user: User): User
     suspend fun findById(id: UUID): User?
     fun getAllUsers(): List<User>
-    suspend  fun updateUser(user: User): User
+    suspend fun updateUser(user: User): User
     fun deleteUser(id: String)
 }
